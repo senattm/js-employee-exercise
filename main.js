c3f4ede5427ef8afc52219d460c8c39f4dc11684
senattm <senaatim95@hotmail.com>
2025-09-23 13:51:25 +0300

    feat(json): 1.1 add JSON stringify function (convertToJSONString)

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const companyData = require("./data/companyData");
+// main.js
+const companyData = require("./data/companyData"); 
+
+// 1.1 - convert companyData to JSON string
+function convertToJSONString(data) {
+  try {
+    const jsonString = JSON.stringify(data);
+    console.log("JSON String conersion succesful");
+    return jsonString;
+  } catch (error) {
+    console.error("JSON conversion error:", error);
+    return null;
+  }
+}
+
+
