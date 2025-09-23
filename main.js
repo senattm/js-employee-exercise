83030362d08fd14637436e63e7885e750a1fce5e
senattm <153332387+senattm@users.noreply.github.com>
2025-09-23 16:20:13 +0300

    git commit -m "feat(json): 1.4 formatted JSON"

--- main.js
+++ main.js
@@ -40,17 +40,4 @@ function printFormattedJSON(data, indent = 2) {
   return formattedJSON;
 }
 
-//2.1 Convert all the employee names to uppercase
-const convertNamesToUpperCase = function(employees){
-    const result = employees.map(emp => ({
-        ...emp,
-        firstName: emp.firstName.toUpperCase(),
-        lastName: emp.lastName.toUpperCase(),
-     } ) );
-
-     console.log("Names uppercased:", 
-        result.slice(0,3).map(e => `${e.firstName} ${e.lastName}`));
-        return result;
-};
-
 
